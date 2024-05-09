9d00ad5080267bda47883c85ebc50e6eb2bac768
Leandro Rossi <leanmatador@gmail.com>
2024-05-09 18:32:12 -0300

    Update: background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,11 +9,13 @@ module.exports = {
 		extend: {
 			colors: {
 				// Personaliza los colores para el modo claro
+				/* lightmode(#a0d9ef,#008aff) gradiente fondo*/
 				light: {
 				  primary: '#ffffff',
 				  text: '#333333',
 				},
 				// Personaliza los colores para el modo oscuro
+				/* darkmode(#051327, #000) gradiente fondo*/
 				dark: {
 				  primary: '#1f2937',
 				  text: '#f9fafb',
