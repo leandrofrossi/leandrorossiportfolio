65a342bfadc282ad8a231c0b903c9b5451d420cf
Leandro Rossi <leanmatador@gmail.com>
2024-09-09 16:32:15 -0300

    Update:lightmode colors

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -39,7 +39,7 @@ const TAGS = {
 
 const ProjectsCard = ({ project }) => {
     const [isModalOpen, setModalOpen] = useState(false);
-    const [selectedProject, setSelectedProject] = useState(null);   
+    const [selectedProject, setSelectedProject] = useState(null);
 
     const openModal = (project) => {
         console.log("Opening modal for project: ", project);
@@ -51,11 +51,11 @@ const ProjectsCard = ({ project }) => {
         console.log("Closing modal");
         setModalOpen(false);
         setSelectedProject(null);        
-    };   
+    };  
 
     return ( 
         <>
-            <div className="cursor-pointer w-full relative overflow-hidden h-[250px] rounded-md group" style={{ backgroundColor: project.bgColor }} onClick={() => openModal(project)}
+            <div className="cursor-pointer w-full relative overflow-hidden h-[250px] rounded-md group text-gray-200" style={{ backgroundColor: project.bgColor }} onClick={() => openModal(project)}
             >
                 <div className="z-10 absolute inset-0 flex items-center p-5 lg:group-hover:pl-0 lg:group-hover:opacity-0 transition-all duration-300 ease-out">
                     <div className="flex flex-col gap-y-2">
@@ -80,7 +80,7 @@ const ProjectsCard = ({ project }) => {
                         </div>
                     </div>
                 </div>
-                <img className="right-2 scale-125 top-5 absolute w-[250px] lg:group-hover:w-[500px]  lg:group-hover:h-auto transition-all duration-700 ease-out" src={project.image} alt="Photo"/>
+                <img className="right-2 scale-125 top-5 absolute w-[250px] lg:right-[-20px] lg:top-11 lg:group-hover:w-[500px]  lg:group-hover:h-auto transition-all duration-700 ease-out" src={project.image} alt={`${project.title} imagen`}/>
                 <div className="h-full aspect-square right-10 absolute lg:group-hover:right-[500px] transition-all duration-700 ease-out" style={{ backgroundImage: `linear-gradient(to right, ${project.bgColor}, transparent)` }}></div>                
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="absolute top-5 right-5 z-10 scale-0 lg:group-hover:scale-100 transition-all duration-500">
                     <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"></path>
@@ -89,14 +89,14 @@ const ProjectsCard = ({ project }) => {
                 </svg>
             </div>            
             {isModalOpen && (                
-                <div className="fixed inset-0 z-50 flex items-center justify-center p-4">
-                    <div className="fixed inset-0 bg-black bg-opacity-80" onClick={closeModal}></div>
-                    <div class="relative mx-auto border-gray-800 bg-gray-800 border-[14px] rounded-[2.5rem] h-[90vh] sm:h-[85vh] max-w-[700px]">
-                        <div class="h-[32px] w-[3px] bg-gray-800 absolute -start-[17px] top-[72px] rounded-s-lg"></div>
-                        <div class="h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
-                        <div class="h-[46px] w-[3px] bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
-                        <div class="h-[64px] w-[3px] bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
-                        <div class="rounded-[2rem] h-[85vh] sm:h-[80vh] bg-gray-800 mt-5">  
+                <div className="fixed inset-0 z-50 flex items-center justify-center p-4 text-gray-200">
+                    <div className="fixed inset-0 bg-gray-400 dark:bg-black bg-opacity-80" onClick={closeModal}></div>
+                    <div class="relative mx-auto border-gray-600 bg-gray-600 dark:border-gray-800 dark:bg-gray-800 border-[14px] rounded-[2.5rem] h-[90vh] sm:h-[85vh] max-w-[600px]">
+                        <div class="h-[32px] w-[3px] bg-gray-600 dark:bg-gray-800 absolute -start-[17px] top-[72px] rounded-s-lg"></div>
+                        <div class="h-[46px] w-[3px] bg-gray-600 dark:bg-gray-800 absolute -start-[17px] top-[124px] rounded-s-lg"></div>
+                        <div class="h-[46px] w-[3px] bg-gray-600 dark:bg-gray-800 absolute -start-[17px] top-[178px] rounded-s-lg"></div>
+                        <div class="h-[64px] w-[3px] bg-gray-600 dark:bg-gray-800 absolute -end-[17px] top-[142px] rounded-e-lg"></div>
+                        <div class="rounded-[2rem] h-[80vh] sm:h-[80vh] bg-gray-600 dark:bg-gray-800 mt-5">  
                             <button onClick={closeModal} className="absolute top-1 right-1 sm:right-3 text-neutrals600"><IoClose className='size-6'/></button>
                                     <h2 className="text-2xl text-center text-pretty font-semibold mb-4">{selectedProject.title}</h2>               
                                     <div className='flex flex-col gap-2 h-5/6 overflow-y-auto scrollbar-style p-2'>
